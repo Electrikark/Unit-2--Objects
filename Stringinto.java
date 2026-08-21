fe9c8b2657680256076ca7419bd5859f28b2c225
Naman Raichura <naman.raichura@gmail.com>
2026-08-21 13:15:06 -0700

    Day 2 or 3 ig string introduction

---
+++ Stringinto.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Stringinto {
+    public static void main(String[] arg){
+        String address = new String("1 Grizzly Way Granite Bay"); // AP test version
+        String location = "Yosemite National Park"; // informal way
+        System.out.println(address);
+        System.out.print(location);
+
+        // Methods for the AP test
+        System.out.println(address.length()); // returns char count
+            // for loops the length will be included in your for loop header for number for iterations
+        
+        // String city = address.substring(14,25);
+            // does not change the string literal
+        // System.out.print();
+        String name = location.substring(9);
+        System.out.println(address.substring(1,4));
+        System.out.println(name);
+
+        int letterLocation = location.indexOf("25"); // String as arg
+        // if no substring or char found, returns -1
+        System.out.println(letterLocation);
+
+        System.out.println(location.equals(address));
+        System.out.println("Yosemite National Park".equals("Yosemite National Park"));
+                            // compares string object
+        String x = new String("hi");
+        String y = new String("hi");
+        System.out.println("hi" == "hi"); // returns
+                        //  x             returns false
+        String z = "hi";
+        System.out.println(z == "hi"); // you can't compare 2 string objects
+        // 2 string objects with formal declartions have own memory
+        // string literals are informal declarations with 
+
+
+        int compare = x.compareTo(y);
+        System.out.println(compare); 
+        int compare_1 = y.compareTo("Hi");
+        System.out.println(compare_1);
+        // doesn't care about the declaration type
+            // positive values mean
+            // argument in compareTo gets subtracted in aski to comparator
+
+        String[] listOfLetters = location.split("a");
+        System.out.println(Arrays.toString(listOfLetters));
+
+
+
+
+
+
+
+
+    }
+}
--- Student.java
+++ Student.java
@@ -17,7 +17,7 @@ public class Student{
         studentID = 000000;
         GPA = 0.0;
         graduate = false;
-
+        numStudents++;
 
     }
 
@@ -27,18 +27,47 @@ public class Student{
         studentID = ID;
         GPA = gpa;
         graduate = grad;
+        numStudents++;
     }
     public Student(String firstname, String lName, int ID, double gpa ){
         this.firstname = firstname;
         lastname = lName;
         studentID = ID;
         GPA = gpa;
+        numStudents++;
+
+    }
+// static - belong to class itself
+// void doesn't return anything
+
+    // setter methods
+    public void setName(String firstName, String lastName){
+        this.firstname = firstName;
+        this.lastname = lastName;
+    }
+
+
+
+    // Overloaded setName
+    public void setName(String firstName ){
+        this.firstname = firstName;
+        this.lastname = "Reassigned last name";
+    }
+
+    // getter methods
+
+    public double getGPA(){
+        return this.GPA;
     }
 
     // 4th methods 
-    public void toString(){
-        System.out.println("");
+    public void printToString(){
+        System.out.println(lastname + ", " + firstname + "with student ID of" + studentID + "has a GPA of " + GPA + "and is a" + graduate + "graduate");
     }
+
+
+
+
     public static void main(String[] args)
 
     {
@@ -47,10 +76,20 @@ public class Student{
         Student num1 = new Student("Naman", "Raichura", 712457, 5.5);
         Student num2 = new Student("Naman", "Raichura", 712457, 5.5, false);
         Student num3 = new Student();
+        num1.setName("Prachi", "Raichura");
+        num1.setName("Prachi" );
 
-        num1.toString(); 
+        num1.printToString(); 
+        // num2.printToString();
+        // num3.printToString();
+                    // inside this class you can access it without calling class name
+        System.out.println("We have "+ numStudents + "Students enrolled");
+
         
 
+
+
+        // Strings
 
     }
 }
